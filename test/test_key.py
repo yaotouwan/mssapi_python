fc5410751009d3d3cdadfedcfbdbd232735000c2
李明扬 <limingyang03@meituan.com>
2016-04-19 18:02:43 +0800

    fix unittest bug when run on swift

--- test/test_key.py
+++ test/test_key.py
@@ -80,12 +80,13 @@ test_util.assert_eq( cont, '2'*1024,  'test set_contents_from_filename')
 os.remove("file_w2")
 
 # test set_contents_from_stream
+k = Key(b, 'key_3')
 fn = 'chunk_file'
-create_file(fn, '1'*1024*1024*10)
+create_file(fn, '1'*1024*10)
 with open(fn, 'r') as fp:
     k.set_contents_from_stream(fp)
 cont =  k.get_contents_as_string()
-test_util.assert_eq(cont, '1'*1024*1024*10, 'test set_contents_from_stream')
+test_util.assert_eq(cont, '1'*1024*10, 'test set_contents_from_stream')
 os.remove(fn)
 
 #test copy
